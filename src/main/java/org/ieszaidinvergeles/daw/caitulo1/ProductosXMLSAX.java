d33bcc3d2339a448ee59c8a62391d7f161a14d5f
Alejandro Alvarez Salas <alvarezsalas.alejandro@gmail.com>
2022-11-29 00:15:38 +0100

    inicial

--- src/main/java/org/ieszaidinvergeles/daw/caitulo1/ProductosXMLSAX.java
+++ src/main/java/org/ieszaidinvergeles/daw/caitulo1/ProductosXMLSAX.java
@@ -4,12 +4,14 @@
  */
 package org.ieszaidinvergeles.daw.caitulo1;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import org.w3c.dom.Document;
 
@@ -37,50 +39,105 @@ public class ProductosXMLSAX extends DefaultHandler{
     
     public Document doc;
 
-    public static void leerYCrear(Path file){
+        int ultimoelement;
+    String cadena_resultado = "";
+    SAXParser parser;
+
+    public ProductosXMLSAX() {
+        ultimoelement = 0;
+
         try {
-            var readerSAX = SAXParserFactory.newInstance()
-                    .newSAXParser()
-                    .getXMLReader();
-            
-            var handler=new SAXHandler();
-            readerSAX.getContentHandler(handler);
-            readerSAX.parse(file);
-            
-        } catch (ParserConfigurationException ex) {
-            Logger.getLogger(ProductosXMLSAX.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SAXException ex) {
-            Logger.getLogger(ProductosXMLSAX.class.getName()).log(Level.SEVERE, null, ex);
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            //Se crea un objeto SAXParser para interpretar el documento XML.
+            parser = factory.newSAXParser();
+            //Se crea una instancia del manejador que será el que recorra el documento 
+            //XML secuencialmente 
+
+        } catch (ParserConfigurationException | SAXException e) {
+            System.err.println("Error " + e.getMessage());
         }
     }
-    
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        //super.characters(ch, start, length); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-        
-        
+
+//    public int abrirXMLSAX(ManejadorSAX sh, SAXParser parser) {
+//        try {
+//            SAXParserFactory factory = SAXParserFactory.newInstance();
+//            //Se crea un objeto SAXParser para interpretar el documento XML.
+//            parser = factory.newSAXParser();
+//            //Se crea una instancia del manejador que será el que recorra el documento 
+//            //XML secuencialmente 
+//            sh = new ManejadorSAX();
+//            return 0;
+//        } catch (ParserConfigurationException | SAXException e) {
+//            System.err.println("Error " + e.getMessage());
+//            return -1;
+//        }
+//    }
+
+    public String recorrerSAX(Path fXML) {
+        try {
+//            SAXParserFactory factory = SAXParserFactory.newInstance();
+            //Se crea un objeto SAXParser para interpretar el documento XML.
+//            SAXParser parser = factory.newSAXParser();
+
+            //Se crea una instancia del manejador que será el que recorra el documento 
+            //XML secuencialmente 
+            parser.parse(fXML.toFile(), this);
+
+            return this.cadena_resultado;
+        } catch (SAXException e) {
+            return "Error al parsear con SAX " + e.getMessage();
+        } catch (IOException ex) {
+            return "Error " + ex.getMessage();
+        }
+
     }
 
+    //Se sobrecarga (redefine) el método startElement
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes atts)
+            throws SAXException {
+
+        switch (qName) {
+            case "libro":
+                cadena_resultado += "Publicado en: "
+                        + atts.getValue(atts.getQName(0)) + "\n ";
+                ultimoelement = 1;
+                break;
+            case "titulo":
+                ultimoelement = 2;
+                cadena_resultado += "\n " + "El título es: ";
+                break;
+            case "autor":
+                ultimoelement = 3;
+                cadena_resultado += "\n " + "El autor es: ";
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    //Cuando en este ejemplo se detecta el final de un elemento <libro>, se pone una línea
+    //discontinua en la salida.
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        super.endElement(uri, localName, qName); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        if (qName.equalsIgnoreCase("libro")) {
+            cadena_resultado = cadena_resultado + "\n -------------------";
+        }
     }
 
     @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        super.startElement(uri, localName, qName, attributes); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void endDocument() throws SAXException {
-        super.endDocument(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
-    @Override
-    public void startDocument() throws SAXException {
-        super.startDocument(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-    
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        if (ultimoelement == 2) {
+            for (int i = start; i < length + start; i++) {
+                cadena_resultado = cadena_resultado + ch[i];
+            }
+        } else if (ultimoelement == 3) {
+            for (int i = start; i < length + start; i++) {
+                cadena_resultado = cadena_resultado + ch[i];
+            }
+        }
+    }    
     
     
 }
